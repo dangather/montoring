1fcff48186c2edbf7a93a47d820ef22fdb14f30e
dangather <dan@gather.tech>
2023-02-24 14:54:28 +0000

    aargh

--- monitortest.py
+++ monitortest.py
@@ -7,6 +7,7 @@ import json
 import subprocess as sp
 from supabase import create_client
 from datetime import datetime, timedelta
+from threads import *
 
 url = os.environ.get("URL")
 key = os.environ.get("KEY")
@@ -14,28 +15,31 @@ sb = create_client(url, key)
 s = sb.table("schedule")
 
 def plog(s):
-    print(f"[+] {s}")
+    print(f"[+] [{datetime.now()}] {s}")
 
 
 def nlog(s):
-    print(f"[-] {s}")
+    print(f"[-] [{datetime.now()}] {s}")
 
 
-class test:
-    def __init__(self, name, time, expected_elapsed):
+class Task:
+
+    running = False
+
+    def __init__(self, name, expected_elapsed):
         self.name = name
-        self.time = time
         self.expected_elapsed = expected_elapsed
     
-    def run(self, c, ee, condition):
+    def run(self, command, condition):
         print(f"[+] running {self.name}...")
+        c = command
+        ee = self.expected_elapsed
         now = pc()
         d = sp.Popen(c, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT).stdout.readlines()
         after = pc()
         elapsed = int(after - now)
         round(float(ee),3)
         round(elapsed,3)
-        print(f"[+] done! operation finished in {elapsed}s")
         for i,e in enumerate(d):
             d[i] = e.decode("utf-8")
         for i in d:
@@ -45,9 +49,9 @@ class test:
             else:
                 flag = False
         if flag:
-            plog("success!")
+            plog(f"success! operation finished in {elapsed}s")
         else:
-            nlog(f"failure: {d}")
+            nlog(f"failure: {d}, operation finished in {elapsed}s")
         if elapsed < ee:
             print(f"[+] finished {ee-elapsed}s earlier than expected!")
         elif elapsed > ee:
@@ -55,43 +59,39 @@ class test:
         else:
             print(f"[+] finished on time!")
 
-def updateschedule():
-    updatedtime = str(datetime.strftime(datetime.now()+timedelta(seconds=10), "%H:%M:%S"))
+def updateschedule(i, r):
     try:
-        sb.table("schedule").update({"scheduled_time": updatedtime}).eq("id", 1).execute()
+        sb.table("schedule").update({"scheduled_time": str(datetime.strftime(datetime.now() + timedelta(seconds=i), "%H:%M:%S"))}).eq("id",r).execute()
         sleep(0.5)
     except json.decoder.JSONDecodeError:
         plog("updated schedule!")
-    except:
-        nlog("an error occured")
+    except Exception as e:
+        nlog(f"an error occured: {e}")
 
-def fetch(selector, i, specific):
-    data = json.loads(s.select(selector).execute().json())["data"][i][specific]
+def sfetch(selector, i):
+    data = json.loads(s.select(selector).execute().json())["data"][i][selector]
+    return data
+
+def fetch(selector):
+    data = json.loads(s.select(selector).execute().json())["data"]
     return data
 
 def main():
-    plog("starting up monitoring system")
-    time = datetime.strptime(json.loads(s.select("scheduled_time").execute().json())["data"][0]["scheduled_time"], "%H:%M:%S").time()
-    while True:
-        current = datetime.now().strftime("%H:%M:%S")
-        print(current, time)
-        if str(current) == str(time):
-            tests = []
-            idd = []
-          
-            for i in json.loads(s.select("id").execute().json())["data"]:
-                idd.append(i["id"])
-            
-            expected_elapsed = json.loads(s.select("expected_elapsed").execute().json())["data"][0]["expected_elapsed"]
-            ping = test("ping", time,expected_elapsed)
-            tests.append([time, expected_elapsed])
-            ping.run(json.loads(s.select("command").execute().json())["data"][0]["command"], expected_elapsed, "Reply")
-            updateschedule()
-            time = datetime.strptime(json.loads(s.select("scheduled_time").execute().json())["data"][0]["scheduled_time"], "%H:%M:%S").time()
-        sleep(1)
-        
+    plog("starting the monitoring system...")
+    commands = fetch("command")
+    tasks = []
+    for i in commands:
+        tasks.append(i["command"])
+    print(tasks)
+    f = 0
+    while f < len(commands):
+        task = Task(sfetch("name", f), sfetch("expected_elapsed", f))
+        task.run(sfetch("command", f), sfetch("pass_condition", f))
 
+        f+=1
+        if f == len(commands):
+            f = 0       
 
 
 if __name__ == "__main__":
-    print(fetch("command", 0, "command"))
+    main()
