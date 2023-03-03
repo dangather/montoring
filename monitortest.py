0f96b4547edd1e1b891165969f45355fa6a5fc27
dangather <dan@gather.tech>
2023-03-03 10:54:52 +0000

    improved scheduling and task fetching

--- monitortest.py
+++ monitortest.py
@@ -7,7 +7,6 @@ import json
 import subprocess as sp
 from supabase import create_client
 from datetime import datetime, timedelta
-from threads import *
 
 url = os.environ.get("URL")
 key = os.environ.get("KEY")
@@ -23,15 +22,12 @@ def nlog(s):
 
 
 class Task:
-
-    running = False
-
     def __init__(self, name, expected_elapsed):
         self.name = name
         self.expected_elapsed = expected_elapsed
     
     def run(self, command, condition):
-        print(f"[+] running {self.name}...")
+        plog(f"running {self.name}...")
         c = command
         ee = self.expected_elapsed
         now = pc()
@@ -49,48 +45,48 @@ class Task:
             else:
                 flag = False
         if flag:
-            plog(f"success! operation finished in {elapsed}s")
+            plog(f"success operation finished in {elapsed}s")
         else:
             nlog(f"failure: {d}, operation finished in {elapsed}s")
         if elapsed < ee:
-            print(f"[+] finished {ee-elapsed}s earlier than expected!")
+            plog(f"finished {ee-elapsed}s earlier than expected")
         elif elapsed > ee:
-            print(f"[-] finished {elapsed-ee}s later than expected.")
+            nlog(f"finished {elapsed-ee}s later than expected.")
         else:
-            print(f"[+] finished on time!")
+            plog(f"finished on time")
 
 def updateschedule(i, r):
     try:
-        sb.table("schedule").update({"scheduled_time": str(datetime.strftime(datetime.now() + timedelta(seconds=i), "%H:%M:%S"))}).eq("id",r).execute()
-        sleep(0.5)
+        sb.table("schedule").update({"scheduled_time": str(datetime.strftime(datetime.now() + timedelta(minutes=i), "%H:%M:%S"))}).eq("id", r).execute()
     except json.decoder.JSONDecodeError:
-        plog("updated schedule!")
+        plog(f"added {i} minutes to {r}")
+        plog("updated schedule")
+        sleep(0.5)
     except Exception as e:
         nlog(f"an error occured: {e}")
 
-def sfetch(selector, i):
-    data = json.loads(s.select(selector).execute().json())["data"][i][selector]
-    return data
-
 def fetch(selector):
-    data = json.loads(s.select(selector).execute().json())["data"]
+    data = json.loads(s.select(selector).order("id").execute().json())["data"]
     return data
 
+def getdata(selector, what, where):
+    return json.loads(s.select(selector).eq(what, where).execute().json())["data"][0][selector]
+
+def t1():
+    scope = tasks if len(tasks) <= 2 else tasks[:2]
+    for f in range(len(scope)):
+        task = Task(getdata("name", "command", scope[f]), getdata("expected_elapsed", "command", scope[f]))
+        task.run(getdata("command", "command", scope[f]), getdata("pass_condition", "command", scope[f]))
+        updateschedule(int(getdata("interval", "command", scope[f])), f)
+  
 def main():
     plog("starting the monitoring system...")
     commands = fetch("command")
+    global tasks
     tasks = []
     for i in commands:
         tasks.append(i["command"])
-    print(tasks)
-    f = 0
-    while f < len(commands):
-        task = Task(sfetch("name", f), sfetch("expected_elapsed", f))
-        task.run(sfetch("command", f), sfetch("pass_condition", f))
-
-        f+=1
-        if f == len(commands):
-            f = 0       
+    t1()
 
 
 if __name__ == "__main__":
