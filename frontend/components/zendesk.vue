3ba5bc9e20c7b9692d37fac1018c2fde68c96a58
dangather <dan@gather.tech>
2023-03-24 13:30:25 +0000

    sadfasdf

--- frontend/components/zendesk.vue
+++ frontend/components/zendesk.vue
@@ -2,15 +2,19 @@
     <div class="p-5 text-center shadow text-5xl">
         Zendesk Tickets
     </div>
-    <div class="flex justify-center">
-    <zcards title="Open tickets" colour="green" border="8px solid green"> 
-            131 
-    </zcards>
-    <Icon name="mdi:youtube" width="50" color="#15CA82"/>
-    <zcards  title="Unassigned" colour="grey" border="8px solid grey"> 
-        14
-    </zcards>
-</div>
+
+    <div class="flex justify-center ">
+        <zcards title="Open tickets" colour="green" border="8px solid green"> 
+                131 
+        </zcards>
+        <zcards  title="Unassigned" colour="grey" border="8px solid grey"> 
+            14
+        </zcards>    
+        <zcards  title="Unsatisfied clients" colour="red" border="8px solid red"> 
+            242423235
+        </zcards>    
+   
+    </div>
 </template>
 
 <script setup>
